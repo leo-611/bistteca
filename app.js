3108840e7431aa205cf6ad16c40d39ca2759884e
leo-611 <almeida.godoy@escola.pr.gov.br>
2025-10-03 11:58:06 -0300

    update

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ let foto_troca = false
 function trocaFoto() {
     if (foto_troca === false) {
         for (let i = 0; i < imagem.length; i++) {
-            imagem[i].src = `img/leon${i + 9}.jpeg`
+            imagem[i].src = `img/leon${i + 9}`
         }
         foto_troca = true;
     } else {
         for (let i = 0; i < imagem.length; i++) {
-            imagem[i].src = `img/leon${i + 1}.jpg`;
+            imagem[i].src = `img/leon${i + 1}`;
         }
         foto_troca = false;
     }
