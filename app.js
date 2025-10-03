08686cc1b41a76ddabc0d94697ec3d53b66d45bd
leo-611 <almeida.godoy@escola.pr.gov.br>
2025-10-03 12:02:06 -0300

    update

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const imagem = document.querySelectorAll('img');
-let foto_troca = false;
+const imagem = document.querySelectorAll('img')
+let foto_troca = false
 
 function trocaFoto() {
     if (foto_troca === false) {
         for (let i = 0; i < imagem.length; i++) {
-            // Troca as imagens, aceitando tanto .jpg quanto .jpeg
-            imagem[i].src = `img/leon${i + 9}.jpg`;  // Começa com .jpg
-            if (imagem[i].src.endsWith('.jpg')) {
-                imagem[i].src = imagem[i].src.replace('.jpg', '.jpeg');  // Se for .jpg, troca para .jpeg
-            }
+            imagem[i].src = `img/leon${i + 9}.jpg`
         }
         foto_troca = true;
     } else {
         for (let i = 0; i < imagem.length; i++) {
-            imagem[i].src = `img/leon${i + 1}.jpg`;  // Começa com .jpg
-            if (imagem[i].src.endsWith('.jpg')) {
-                imagem[i].src = imagem[i].src.replace('.jpg', '.jpeg');  // Troca para .jpeg
-            }
+            imagem[i].src = `img/leon${i + 1}.jpg`;
         }
         foto_troca = false;
     }
